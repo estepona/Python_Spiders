a801b46655f4972ab3844f729c2379c4a749699a
Freddie Zhang <574953550@qq.com>
2017-04-07 23:38:25 -0400

    Add files via upload

--- 6 YouTube Trending Videos Analysis/YouTube_Trending_Spider.py
+++ 6 YouTube Trending Videos Analysis/YouTube_Trending_Spider.py
@@ -43,19 +43,34 @@ def get_info():
                 videos['title'].append(i_api['items'][0]['snippet']['title'])
                 videos['channel'].append(i_api['items'][0]['snippet']['channelTitle'])
                 videos['datetime'].append(i_datetime)
-                videos['viewCount'].append(i_api['items'][0]['statistics']['viewCount'])
-                videos['likeCount'].append(i_api['items'][0]['statistics']['likeCount'])
-                videos['dislikeCount'].append(i_api['items'][0]['statistics']['dislikeCount'])
-                videos['commentCount'].append(i_api['items'][0]['statistics']['commentCount'])
-                videos['oneDayEnd'].append(0)
-
+                # if there is 'statistics'
+                try:
+                    if_s_exists = i_api['items'][0]['statistics']['viewCount']
+                except KeyError:
+                    if_s_exists = 'gotothatpage'
+                # if 'statistics' is unavailable
+                if if_s_exists == 'gotothatpage':
+                    i_tough = BS(requests.get('http://' + i_url).content, 'html.parser')
+                    videos['viewCount'].append(i_tough.find('div', {'class':'watch-view-count'}).get_text()[:-6].replace(',',''))
+                    videos['likeCount'].append(i_tough.find('button', {'title':'Unlike'}).get_text().replace(',',''))
+                    videos['dislikeCount'].append(i_tough.find('button', {'title':'I dislike this'}).get_text().replace(',',''))
+                    videos['commentCount'].append('')
+                    videos['oneDayEnd'].append(0)
+                # if 'statistics' is available
+                else:
+                    videos['viewCount'].append(i_api['items'][0]['statistics']['viewCount'])
+                    videos['likeCount'].append(i_api['items'][0]['statistics']['likeCount'])
+                    videos['dislikeCount'].append(i_api['items'][0]['statistics']['dislikeCount'])
+                    videos['commentCount'].append(i_api['items'][0]['statistics']['commentCount'])
+                    videos['oneDayEnd'].append(0)
+                    
 def to_pd(videos):
     df = pd.DataFrame(videos)
     df = df[col_list]
     return df
 
 def to_csv(df):
-    df.to_csv('Trending Videos.csv')
+    df.to_csv('Trending Videos.csv', encoding='utf-8')
 
 def add_new(df):
     # clean df
@@ -76,17 +91,37 @@ def add_new(df):
                 i_api = requests.get(api_url.replace('here', i_id)).json()
                 i_datetime = pd.to_datetime(i_api['items'][0]['snippet']['publishedAt']) - pd.Timedelta('4 hours')
                 if (time_now - i_datetime).days < 1:
-                    df.loc[-1] = [i_api['items'][0]['snippet']['title'], 
-                                  i_id, 
-                                  'www.youtube.com' + td[i].find('a').get('href'), 
-                                  i_api['items'][0]['snippet']['channelTitle'],
-                                  i_datetime,
-                                  td[i].find('span',{'class':'video-time'}).get_text(),
-                                  i_api['items'][0]['statistics']['viewCount'],
-                                  i_api['items'][0]['statistics']['likeCount'],
-                                  i_api['items'][0]['statistics']['dislikeCount'],
-                                  i_api['items'][0]['statistics']['commentCount'],
-                                  0]
+                    i_url = 'www.youtube.com' + td[i].find('a').get('href')
+                    # if there is 'statistics'
+                    try:
+                        if_s_exists = i_api['items'][0]['statistics']['viewCount']
+                    except KeyError:
+                        if_s_exists = 'gotothatpage'
+                    if if_s_exists == 'gotothatpage':
+                        i_tough = BS(requests.get('http://' + i_url).content, 'html.parser')
+                        df.loc[-1] = [i_api['items'][0]['snippet']['title'], 
+                                      i_id, 
+                                      i_url, 
+                                      i_api['items'][0]['snippet']['channelTitle'],
+                                      i_datetime,
+                                      td[i].find('span',{'class':'video-time'}).get_text(),
+                                      i_tough.find('div', {'class':'watch-view-count'}).get_text()[:-6].replace(',',''),
+                                      i_tough.find('button', {'title':'Unlike'}).get_text().replace(',',''),
+                                      i_tough.find('button', {'title':'I dislike this'}).get_text().replace(',',''),
+                                      '',
+                                      0]
+                    else:
+                        df.loc[-1] = [i_api['items'][0]['snippet']['title'], 
+                                      i_id, 
+                                      i_url, 
+                                      i_api['items'][0]['snippet']['channelTitle'],
+                                      i_datetime,
+                                      td[i].find('span',{'class':'video-time'}).get_text(),
+                                      i_api['items'][0]['statistics']['viewCount'],
+                                      i_api['items'][0]['statistics']['likeCount'],
+                                      i_api['items'][0]['statistics']['dislikeCount'],
+                                      i_api['items'][0]['statistics']['commentCount'],
+                                      0]
                     df = df.reset_index(drop=True)
     return df
 
@@ -97,14 +132,27 @@ def check_old(df):
         df_time = pd.to_datetime(df_time)
         if df.oneDayEnd.iloc[df_time_index] == 0:
             if (time_now - df_time).days >= 1:
+                i_api = api_url.replace('here', df.ID.iloc[df_time_index]).json()
+                i_url = df.url.iloc[df_time_index]
                 # update 
-                api_url = 'https://www.googleapis.com/youtube/v3/videos?id=here&part=snippet,statistics,recordingDetails&key=AIzaSyA_ltEFFYL4E_rOBYkQtA8aKHnL5QR_uMA'
-                i_api = api_url.replace('here', df.ID.iloc[df_time_index])
-                df.set_value(df_time_index, 'viewCount', i_api['items'][0]['statistics']['viewCount'])
-                df.set_value(df_time_index, 'likeCount', i_api['items'][0]['statistics']['likeCount'])
-                df.set_value(df_time_index, 'dislikeCount', i_api['items'][0]['statistics']['dislikeCount'])
-                df.set_value(df_time_index, 'commentCount', i_api['items'][0]['statistics']['commentCount'])
-                df.set_value(df_time_index, 'oneDayEnd', 1)
+                # if there is 'statistics'
+                try:
+                    if_s_exists = i_api['items'][0]['statistics']['viewCount']
+                except KeyError:
+                    if_s_exists = 'gotothatpage'
+                if if_s_exists == 'gotothatpage':    
+                    i_tough = BS(requests.get('http://' + i_url).content, 'html.parser')
+                    df.set_value(df_time_index, 'viewCount', i_tough.find('div', {'class':'watch-view-count'}).get_text()[:-6].replace(',',''))
+                    df.set_value(df_time_index, 'likeCount', i_tough.find('button', {'title':'Unlike'}).get_text().replace(',',''))
+                    df.set_value(df_time_index, 'dislikeCount', i_tough.find('button', {'title':'I dislike this'}).get_text().replace(',',''))
+                    # no need for comment since alreay '' in this case
+                    df.set_value(df_time_index, 'oneDayEnd', 1)
+                else:    
+                    df.set_value(df_time_index, 'viewCount', i_api['items'][0]['statistics']['viewCount'])
+                    df.set_value(df_time_index, 'likeCount', i_api['items'][0]['statistics']['likeCount'])
+                    df.set_value(df_time_index, 'dislikeCount', i_api['items'][0]['statistics']['dislikeCount'])
+                    df.set_value(df_time_index, 'commentCount', i_api['items'][0]['statistics']['commentCount'])
+                    df.set_value(df_time_index, 'oneDayEnd', 1)
     return df
 
 
@@ -131,17 +179,17 @@ if not old_file_name.is_file():
     to_csv(to_pd(videos))
 
 if not new_file_name.is_file():
-    df = pd.read_csv('Trending Videos.csv')
+    df = pd.read_csv('Trending Videos.csv', encoding='utf-8')
     df = add_new(df)
     df = check_old(df)
-    df.to_csv('Trending Videos_new.csv')
+    df.to_csv('Trending Videos_new.csv', encoding='utf-8')
 else:
     # update two files
-    df = pd.read_csv('Trending Videos_new.csv')
+    df = pd.read_csv('Trending Videos_new.csv', encoding='utf-8')
     df = add_new(df)
     df = check_old(df)
-    backup = pd.read_csv('Trending Videos_new.csv')
+    backup = pd.read_csv('Trending Videos_new.csv', encoding='utf-8')
     os.remove('Trending Videos.csv')
-    backup.to_csv('Trending Videos.csv')
+    backup.to_csv('Trending Videos.csv', encoding='utf-8')
     os.remove('Trending Videos_new.csv')
-    df.to_csv('Trending Videos_new.csv')
+    df.to_csv('Trending Videos_new.csv', encoding='utf-8')
